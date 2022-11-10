44b7509674bee2adda988c3920000198c9cda72f
Ashish Guleria <54684192+codeashish@users.noreply.github.com>
2022-11-10 17:53:56 +0530

    Add files via upload
    Init

---
+++ shivamkiBandiKeBhaiKaProject/main.js
@@ -0,0 +1,34 @@
+const express = require('express');
+var fs = require('fs');
+const app = express();
+const port = 9000;
+const unzipper = require('unzipper');
+const multer = require('multer');
+const figlet = require('figlet');
+const gradient = require('gradient-string');
+const colors = require('colors');
+const upload = multer({
+  dest: 'uploads/',
+});
+
+app.post('/zip', upload.single('zipFile'), async function (req, res, next) {
+  try {
+    await fs.createReadStream(req.file.path).pipe(
+      unzipper.Extract({
+        path: 'output',
+      })
+    );
+    console.log(
+      colors.rainbow.underline(`${req.file.originalname} is unzipped`)
+    );
+    res.send(`${req.file.originalname} is unzipped`);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(
+    gradient.pastel.multiline(figlet.textSync(`Server started on port ${port}`))
+  );
+});
